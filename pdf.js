a07bf70a8eb0950aee9bb06696add9fb1ddcd9f8
Charles Eldridge <charles@charleseldridge.com>
2019-09-20 17:44:52 +1000

    Errorchecking

--- pdf.js
+++ pdf.js
@@ -2,15 +2,18 @@ function strip(){
   var students=prompt("Paste the list");
   var band=prompt("Paste the band");
   var studentarray=students.split(" ");
+  console.log(studentarray);
   for (var i = 0; i < studentarray.length; i++) {
     if (/\d/.test(studentarray[i]) === true) {
       studentarray[i]= null;
     }
   }
+  console.log(studentarray);
   var filtered = studentarray.filter(
     function (el) {
       return el != null;
     }
   );
+  console.log(filtered);
   document.getElementById("New").value=filtered;
 }
