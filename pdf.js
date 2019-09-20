5e06f4c04aa141b4ab078aad26371efaaa6128f1
Charles Eldridge <charles@charleseldridge.com>
2019-09-20 17:37:38 +1000

    Create pdf.js

---
+++ pdf.js
@@ -0,0 +1,16 @@
+function strip(){
+  var students=prompt("Paste the list");
+  var band=prompt("Paste the band");
+  var studentarray=students.split(" ");
+  for (var i = 0; i < studentarray.length; i++) {
+    if (/\d/.test(studentarray[i]) === true) {
+      studentarray[i]= null;
+    }
+  }
+  var filtered = studentarray.filter(
+    function (el) {
+      return el != null;
+    }
+  );
+  console.log(filtered);
+}
